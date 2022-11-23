6b4d17b83355b8a4a88aba2cf31a1458c89bdd70
afekTheMiniLearner <afeksa2000@gmail.com>
2022-11-23 18:11:03 +0200

    play login organized

--- scripts/play-logic.js
+++ scripts/play-logic.js
@@ -2,66 +2,83 @@ const _sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
 async function play(item) {
   if (inPlaying) return;
-
   inPlaying = true;
+
   _disableActions();
 
-  playerChoose = item;
-  const playerElementId = {
-    [ITEMS.ROCK]: DOM_SELECTORS_IDS.PLAYER_ACTIONS.ROCK,
-    [ITEMS.PAPER]: DOM_SELECTORS_IDS.PLAYER_ACTIONS.PAPER,
-    [ITEMS.SCISSORS]: DOM_SELECTORS_IDS.PLAYER_ACTIONS.SCISSORS,
-  }[item];
-  const playerElement = document.getElementById(playerElementId);
-  playerElement.classList.add("active");
+  showChoosenItemPlayer();
 
-  // computer choosing
   const computerChoose = await _computerPlay();
-  const computerActionElement = document.getElementById(
-    DOM_SELECTORS_IDS.COMPUTER_ACTION
-  );
-  computerActionElement.classList.add(CLASS_ITEM[computerChoose]);
 
+  showChoosenItemComputer();
+
+  playerChoose = item;
   const winnerPlayer = _getWinner(
     { player: 1, choose: playerChoose },
     { player: 2, choose: computerChoose }
   );
 
-  const winnerContainerElement = document.getElementById(
-    DOM_SELECTORS_IDS.WINNER_CONTAINER
-  );
-  const idleContainerElement = document.getElementById(
-    DOM_SELECTORS_IDS.IDLE_CONTAINER
-  );
+  showWinnerContainer();
 
-  winnerContainerElement.style.display = "flex";
+  updateScoreDisplay();
 
-  idleContainerElement.style.display = "none";
+  function showChoosenItemPlayer() {
+    const playerElementId = {
+      [ITEMS.ROCK]: DOM_SELECTORS_IDS.PLAYER_ACTIONS.ROCK,
+      [ITEMS.PAPER]: DOM_SELECTORS_IDS.PLAYER_ACTIONS.PAPER,
+      [ITEMS.SCISSORS]: DOM_SELECTORS_IDS.PLAYER_ACTIONS.SCISSORS,
+    }[item];
 
-  totalGames++;
+    const playerElement = document.getElementById(playerElementId);
 
-  const winnerElement = document.getElementById(DOM_SELECTORS_IDS.WINNER);
-  switch (winnerPlayer) {
-    case 1:
-      winnerElement.innerText = "You";
-      score.updateScore(score.getScore() + 1);
-      break;
-    case 2:
-      winnerElement.innerText = "Computer";
-      break;
-    case 0:
-    default:
-      winnerElement.innerText = "Draw";
+    playerElement.classList.add("active");
   }
 
-  const totalElement = document.getElementById(DOM_SELECTORS_IDS.TOTAL_GAME);
-  totalElement.innerText = `${totalGames}`;
+  function showChoosenItemComputer() {
+    const computerActionElement = document.getElementById(
+      DOM_SELECTORS_IDS.COMPUTER_ACTION
+    );
+    computerActionElement.classList.add(CLASS_ITEM[computerChoose]);
+  }
+
+  function showWinnerContainer() {
+    const winnerContainerElement = document.getElementById(
+      DOM_SELECTORS_IDS.WINNER_CONTAINER
+    );
+    winnerContainerElement.style.display = "flex";
+
+    const idleContainerElement = document.getElementById(
+      DOM_SELECTORS_IDS.IDLE_CONTAINER
+    );
+    idleContainerElement.style.display = "none";
+
+    totalGames++;
+  }
+
+  function updateScoreDisplay() {
+    const winnerElement = document.getElementById(DOM_SELECTORS_IDS.WINNER);
+    switch (winnerPlayer) {
+      case 1:
+        winnerElement.innerText = "You";
+        score.updateScore(score.getScore() + 1);
+        break;
+      case 2:
+        winnerElement.innerText = "Computer";
+        break;
+      case 0:
+      default:
+        winnerElement.innerText = "Draw";
+    }
+
+    const totalElement = document.getElementById(DOM_SELECTORS_IDS.TOTAL_GAME);
+    totalElement.innerText = `${totalGames}`;
+  }
 }
 
 async function _computerPlay() {
   const options = Object.values(ITEMS);
-  const r = Math.floor(Math.random() * 100);
-  const random = options[r % options.length];
+  const randomNumber = Math.floor(Math.random() * 100);
+  const randomItem = options[randomNumber % options.length];
 
   const computerActionElement = document.getElementById(
     DOM_SELECTORS_IDS.COMPUTER_ACTION
@@ -71,5 +88,5 @@ async function _computerPlay() {
   await _sleep(1500);
   computerActionElement.classList.remove("spin");
 
-  return random;
+  return randomItem;
 }
